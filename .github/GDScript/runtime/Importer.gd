433370363be7868537098ae7e833242f93022d7c
kingzachariah <videokingfour@gmail.com>
2024-07-09 11:27:43 +0100

    Revert "redo"
    This reverts commit 1551fca94135b136c434c82ffd73701c40fb7e06.

---
+++ .github/GDScript/runtime/Importer.gd
@@ -0,0 +1,31 @@
+# MIT License
+#
+# Copyright (c) 2023 Roland Helmerichs
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+func import(source_file: String, project_file: String = ""):
+	var tilemapCreator = preload("TilemapCreator.gd").new()
+	tilemapCreator.set_map_layers_to_tilemaps(true)
+	tilemapCreator.set_add_class_as_metadata(true)
+	if project_file != "":
+		var ct = preload("CustomTypes.gd").new()
+		ct.load_custom_types(project_file)
+		tilemapCreator.set_custom_types(ct)
+	return tilemapCreator.create(source_file)
